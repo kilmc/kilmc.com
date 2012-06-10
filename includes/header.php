3f0b55621c48bd5ab5337e05a6691535e29b8c92
Kilian McMahon <kil@kilmc.com>
2012-06-10 17:05:15 +0100

    Further styling and adding responsive code

--- includes/header.php
+++ includes/header.php
@@ -1,5 +1,5 @@
 <header class="container">
-  <h1><a class="logo ir" href="/kilmc/git/kilmc/">Kilmc</a></h1>
+  <h1><a class="logo ir" href="/git/kilmc/">Kilmc</a></h1>
   <ul class="top-nav clearfix">
     <li><a href="http://twitter.com/kilmc">Twitter</a></li>
     <li><a href="/git/kilmc/projects">Projects</a></li>
--- projects/index.php
+++ projects/index.php
@@ -6,36 +6,37 @@
   </head>
   <body>
     <?php include('../includes/header.php'); ?>
+    <div class="projects-container">
+      <div class="container project-wrap">
+        <a href="#"><img src="../images/ull-project.png" /></a>
+        <a href="#"><h2 class="title">Ull Conference</h2></a>
+        <ul class="project-tags">
+          <li class="project-tag"><a href="#">Branding</a></li>
+          <li class="project-tag"><a href="#">Web</a></li>
+          <li class="project-tag"><a href="#">Print</a></li>
+        </ul>
 
-    <div class="container project-wrap">
-      <a href="#"><img src="../images/ull-project.png" /></a>
-      <a href="#"><h2 class="title">Ull Conference</h2></a>
-      <ul class="project-tags">
-        <li class="project-tag"><a href="#">Branding</a></li>
-        <li class="project-tag"><a href="#">Web</a></li>
-        <li class="project-tag"><a href="#">Print</a></li>
-      </ul>
+      </div>
 
-    </div>
+      <div class="container project-wrap">
+        <a href="#"><img src="../images/001Hundred.png" /></a>
+        <a href="#"><h2 class="title">Hundred Project</h2></a>
+        <ul class="project-tags">
+          <li class="project-tag"><a href="#">Photography</a></li>
+          <li class="project-tag"><a href="#">Design</a></li>
+        </ul>
 
-    <div class="container project-wrap">
-      <a href="#"><img src="../images/001Hundred.png" /></a>
-      <a href="#"><h2 class="title">Hundred Project</h2></a>
-      <ul class="project-tags">
-        <li class="project-tag"><a href="#">Photography</a></li>
-        <li class="project-tag"><a href="#">Design</a></li>
-      </ul>
+      </div>
 
-    </div>
+      <div class="container project-wrap">
+        <a href="#"><img src="../images/Antlers_T-01.png" /></a>
+        <a href="#"><h2 class="title">Sketches & Extras</h2></a>
+        <ul class="project-tags">
+          <li class="project-tag"><a href="#">Illustration</a></li>
+          <li class="project-tag"><a href="#">College</a></li>
+        </ul>
 
-    <div class="container project-wrap">
-      <a href="#"><img src="../images/Antlers_T-01.png" /></a>
-      <a href="#"><h2 class="title">Sketches & Extras</h2></a>
-      <ul class="project-tags">
-        <li class="project-tag"><a href="#">Illustration</a></li>
-        <li class="project-tag"><a href="#">College</a></li>
-      </ul>
-
+      </div>
     </div>
   </body>
 </html>
