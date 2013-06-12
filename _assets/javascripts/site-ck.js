83979d866e15664c1b3eb0d59d9964a2c1390e2f
Kilian McMahon <kil@kilmc.com>
2013-06-12 13:12:05 +0200

    Basic styling

---
+++ _assets/javascripts/site-ck.js
@@ -0,0 +1,1 @@
+$(document).ready(function(){$(".logo").click(function(){$(".container").fadeToggle("fast",function(){});$(".menu").fadeToggle("fast",function(){})})});
---
+++ _assets/javascripts/site.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+  $(".logo").click(function () {
+    $(".container").fadeToggle("fast", function () {
+
+    });
+    $(".menu").fadeToggle("fast", function () {
+      
+    });
+  });
+});
