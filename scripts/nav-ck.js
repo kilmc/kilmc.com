8db2f395beb1be46be62c106515d452cdba46a86
Kilian McMahon <kil@kilmc.com>
2013-10-01 13:44:56 -0700

    Fix lots of things, add jessica page

--- scripts/nav-ck.js
+++
@@ -1,1 +0,0 @@
-$(document).ready(function(){$(".logo").click(function(){$(".content-wrap, .site-header, .site-footer").fadeToggle("fast",function(){});$(".site-page-list").fadeToggle("fast",function(){})})});
